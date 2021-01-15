e8fb2ba6324c78f40037aef6dfae1e4dc8c8a60b
garret123h <garret12@uw.edu>
2021-01-14 16:22:42 -1000

    Added image URL reading

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, jsonify, request
-import functools, sys, cv2, numpy
 from flask_restful import Api, Resource, reqparse
+from skimage import io
+import functools, sys, cv2, numpy
 
 app = Flask(__name__)
 api = Api(app)
 
 def ok_user_and_password(username, password):
-    print('Saved username --> ', app.config['USERNAME'])
-    print('Saved password --> ', app.config['PASSWORD'])
     return username == app.config['USERNAME'] and password == app.config['PASSWORD']
 
 def authenticate():
@@ -34,8 +33,17 @@ class img_comp(Resource):
     @requires_authorization
     def get(self, id):
         images = request.get_json()
-        image_one = cv2.imread(images["image-one"])
-        image_two = cv2.imread(images["image-two"])
+        image_one = image_two = None
+
+        if "https://" in images["image-one"]:
+            print('Doing something')
+            image_one = io.imread(images["image-one"])
+        else:
+            image_one = cv2.imread(images["image-one"])
+        if "https://" in images["image-two"]:
+            image_two = io.imread(images["image-two"])
+        else:
+            image_two = cv2.imread(images["image-two"])
 
         comp_percent = self.compare_images(image_one, image_two)
         
